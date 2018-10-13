cb198c9da4fcfa30dcd1fad6a60a37a31bdb8a1d
Igor Khripunov <igor.khripunov@spidertracks.co.nz>
2018-10-13 19:51:25 +1300

    Adds main.js with desired dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Customer from './components/Customer.vue'
+import Note from './components/Note.vue'
+import bDropdown from 'bootstrap-vue/es/components/dropdown/dropdown';
+import bDropdownItem from 'bootstrap-vue/es/components/dropdown/dropdown-item';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.component('b-dropdown', bDropdown);
+Vue.component('b-dropdown-item', bDropdownItem);
 
 Vue.config.productionTip = false
-
+Vue.component("Customer", Customer);
+Vue.component("Note", Note);
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  data: {
+
+  }
 }).$mount('#app')
